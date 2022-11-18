29ff9cfafc09ed35ee9ddca2d996bacae58f103b
LeonardoArrudaa <leonardo.arruda@aluno.ifsp.edu.br>
2022-11-17 21:26:12 -0300

    Adição de buscas: por mercado e nome, categoria e nome, range de preço

--- src/main/java/br/edu/ifsp/arq/prss6/apieconomarket/repository/ProductRepository.java
+++ src/main/java/br/edu/ifsp/arq/prss6/apieconomarket/repository/ProductRepository.java
@@ -10,10 +10,17 @@ import br.edu.ifsp.arq.prss6.apieconomarket.domain.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-	@Query(value = "SELECT p FROM product p where p.searchName LIKE %:name%")
+	@Query(value = "SELECT p FROM product p WHERE p.searchName LIKE %:name%")
 	public Page<Product> findBySearchNameLike(@Param("name") String name, Pageable pagination);
+	
+	@Query(value = "SELECT p FROM product p WHERE p.price BETWEEN :minPrice AND :maxPrice")
+	public Page<Product> findByPriceRange(@Param("minPrice")Double minPrice, @Param("maxPrice")Double maxPrice, Pageable pagination);
 	
 	public Page<Product> findByMarketsId(Long id, Pageable pagination);
 	
+	public Page<Product> findByMarketsIdAndSearchNameLike(Long id, String name, Pageable pagination);
+	
 	public Page<Product> findByCategoryId(Long id, Pageable pagination);
+	
+	public Page<Product> findByCategoryIdAndSearchNameLike(Long id, String name, Pageable pagination);
 }
